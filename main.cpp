230e47360935a665e02abaa3c8a9fa93fb7b9625
neevlila <141387755+neevlila@users.noreply.github.com>
2023-08-04 10:03:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,66 +1,25 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-// Function to insert an element into the array
-void insert(int arr[], int size, int pos, int value) {
-    if (pos < 0 || pos > size) {
-        printf("Invalid position for insertion!\n");
-        return;
-    }
-
-    // Shift elements to the right to make space for the new value
-    for (int i = size - 1; i >= pos; i--) {
-        arr[i + 1] = arr[i];
-    }
-
-    // Insert the value at the specified position
-    arr[pos] = value;
-}
-
-// Function to delete an element from the array
-void delete(int arr[], int size, int pos) {
-    if (pos < 0 || pos >= size) {
-        printf("Invalid position for deletion!\n");
-        return;
-    }
-
-    // Shift elements to the left to remove the element at the specified position
-    for (int i = pos; i < size - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
+bool isVowel(char ch) {
+    ch = tolower(ch); // Convert character to lowercase for case-insensitivity
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
 }
 
 int main() {
-    int arr[100], size, pos, value;
-    
-    printf("Enter the size of the array: ");
-    scanf("%d", &size);
+    char character;
+    cout << "Enter a character: ";
+    cin >> character;
 
-    printf("Enter the elements of the array:\n");
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
+    if (isalpha(character)) {
+        if (isVowel(character)) {
+            cout << character << " is a vowel." << endl;
+        } else {
+            cout << character << " is a consonant." << endl;
+        }
+    } else {
+        cout << "Invalid input. Please enter an alphabet character." << endl;
     }
-
-    printf("Enter the position for insertion: ");
-    scanf("%d", &pos);
-    printf("Enter the value to be inserted: ");
-    scanf("%d", &value);
-    insert(arr, size, pos, value);
-
-    printf("Array after insertion:\n");
-    for (int i = 0; i < size + 1; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    printf("Enter the position for deletion: ");
-    scanf("%d", &pos);
-    delete(arr, size + 1, pos);
-
-    printf("Array after deletion:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
 
     return 0;
 }
